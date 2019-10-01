d006e4b7c2ff7e61b9a716481daf0d81afbc15b0
kaminora <kaminoura9207@gmail.com>
2019-10-01 21:44:12 +0900

    const

---
+++ const/const.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+
+template <typename Array>
+void print( Array & c )
+{
+  for (std::size_t i = 0 ; i != c.size() ; ++i)
+  {
+    std::cout << c[i];
+  }
+}
+
+int main()
+{
+  std::array<int, 5> a = { 1, 2, 3, 4, 5} ;
+  print(a);
+}
