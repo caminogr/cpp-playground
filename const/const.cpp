8e60eebbc9891acaeb790a6e91bc284ec1b069ff
kaminora <kaminoura9207@gmail.com>
2019-10-01 22:48:20 +0900

    const

--- const/const.cpp
+++ const/const.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
-#include <array>
+// #include <array>
 
-template <typename Array>
-void print( Array & c )
+// template <typename Array>
+// void print( Array & c )
+// {
+  // for (std::size_t i = 0 ; i != c.size() ; ++i)
+  // {
+    // std::cout << c[i];
+  // }
+// }
+
+// int main()
+// {
+  // std::array<int, 5> a = { 1, 2, 3, 4, 5} ;
+  // print(a);
+// }
+
+// ------------------
+
+template <typename T, std::size_t N>
+struct array
+{
+  using size_type = std::size_t ;
+
+  size_type size() ;
+
+  T storage[N] ;
+  T & operator [] (std::size_t i)
+  {
+    return storage[i] ;
+  }
+} ;
+
+template <typename Container>
+void print( Container const & c )
 {
   for (std::size_t i = 0 ; i != c.size() ; ++i)
   {
@@ -12,6 +43,7 @@ void print( Array & c )
 
 int main()
 {
-  std::array<int, 5> a = { 1, 2, 3, 4, 5} ;
+  array<int, 5> a = { 1, 2, 3, 4, 5} ;
   print(a);
 }
+
