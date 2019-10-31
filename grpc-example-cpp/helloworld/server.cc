e3d09b4903bf7e4fff3d4465fcba8c2f0e457ba0
kaminora <kaminoura9207@gmail.com>
2019-10-31 10:58:14 +0900

    Enable to make grpc.pb

--- grpc-example-cpp/helloworld/server.cc
+++ grpc-example-cpp/helloworld/server.cc
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <string>
 
+// hello.grpc.pb.hはどうやって作る？
+// hello.pb.hではないの？
+// #include "hello.grpc.pb.h"
+
+// using hello::Greeter;
+
+// class GreeterServiceImpl final : publc Greeter::Service {
+
+// }
+
 void RunServer() {
   std::string server_address("0.0.0.0:50051");
+  // GreeterServiceImpl service;
+
+
   std::cout << "Server listening on " << server_address << std::endl;
 }
 
