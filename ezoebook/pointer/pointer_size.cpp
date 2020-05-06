8dc9e5998231e271e68ccccf5ba9fd736d1e04e8
kaminora <kaminoura9207@gmail.com>
2020-05-06 19:54:02 +0900

    Add

--- ezoebook/pointer/pointer_size.cpp
+++ ezoebook/pointer/pointer_size.cpp
@@ -5,7 +5,7 @@ struct hoge {
   bool c = true;
 };
 
-struct fuga {
+struct fuga_struct {
   int a = 0;
   bool b = true;
   bool c = true;
@@ -13,6 +13,14 @@ struct fuga {
   bool e = true;
   bool f = true;
   char name[100 + 1];
+};
+
+class piyo {
+
+  char name[100 + 1];
+};
+class poyo {
+  char name[1000 + 1];
 };
 
 int main(int argc, char *argv[]) {
@@ -29,8 +37,9 @@ int main(int argc, char *argv[]) {
 
   struct hoge *hoge;
   struct hoge *pointer_hoge;
-  struct fuga fuga;
-  struct fuga *pointer_fuga;
+  struct fuga_struct fuga;
+  struct fuga_struct *pointer_fuga;
+  struct std::unique_ptr<fuga_struct> unique_ptr_fuga(new fuga_struct);
   // std::cout << "hoge: " << &hoge << std::endl;
   // std::cout << "hoge c: " << hoge->c << std::endl;
 
@@ -46,11 +55,14 @@ int main(int argc, char *argv[]) {
   std::cout << "&hoge size" << sizeof(&hoge) << std::endl;
   std::cout << "fuga size" << sizeof(fuga) << std::endl;
   std::cout << "pointer fuga size" << sizeof(pointer_fuga) << std::endl;
-  std::cout << "struct fuga size" << sizeof(struct fuga) << std::endl;
+  std::cout << "struct fuga size" << sizeof(struct fuga_struct) << std::endl;
+  std::cout << "unique_ptr_fuga size" << sizeof(unique_ptr_fuga) << std::endl;
+  std::cout << "*unique_ptr_fuga size" << sizeof(*unique_ptr_fuga) << std::endl;
   // std::cout << "struct pointer_fuga size" << sizeof(struct pointer_fuga) <<
   // std::endl;
   std::cout << "&fuga size" << sizeof(&fuga) << std::endl;
   std::cout << "pointer_fuga size" << sizeof(pointer_fuga) << std::endl;
+  std::cout << "*pointer_fuga size" << sizeof(*pointer_fuga) << std::endl;
 
   void *ptr = std::malloc(9);
   void *ptr1 = std::malloc(1);
@@ -61,6 +73,16 @@ int main(int argc, char *argv[]) {
   char str[16];
   std::cout << "str size" << sizeof(str) << std::endl;
   std::cout << "&str size" << sizeof(&str) << std::endl;
+
+  // std::unique_ptr<piyo, std::function<void(poyo *)>> uni_ptr;
+  // std::cout << "piyo size" << sizeof(piyo) << std::endl;
+  // std::cout << "poyo size" << sizeof(poyo) << std::endl;
+  // std::cout << "uni_ptr size" << sizeof(uni_ptr) << std::endl;
+
+  // char tmp[1000 + 1];
+  // char *tmp[1000 + 1];
+  // std::cout << "tmp size" << sizeof(tmp) << std::endl;
+  // std::cout << "tmp size" << sizeof(tmp) << std::endl;
 
   return 0;
 }
